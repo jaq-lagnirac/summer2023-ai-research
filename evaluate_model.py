0e4911c1f0753c32958ccbb225d52fc4c1c56224
Justin Caringal <jacfriday10th@gmail.com>
2023-06-26 15:44:09 -0500

    Initialize evaluate_model.py
    -Will implement evaluate() function from Keras to measure efficacy of
     model

---
+++ evaluate_model.py
@@ -0,0 +1,38 @@
+# Justin Caringal
+# Tests HDF5 model generated from tensorflow
+
+import os, sys; sys
+import argparse
+import logging
+
+SCRIPT_PATH = os.path.abspath(__file__)
+FORMAT = '[%(asctime)s] %(levelname)s %(message)s'
+l = logging.getLogger()
+lh = logging.StreamHandler()
+lh.setFormatter(logging.Formatter(FORMAT))
+l.addHandler(lh)
+l.setLevel(logging.INFO)
+debug = l.debug; info = l.info; warning = l.warning; error = l.error
+
+DESCRIPTION = '''
+'''
+
+EPILOG = '''
+'''
+
+class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
+    argparse.RawDescriptionHelpFormatter):
+  pass
+parser = argparse.ArgumentParser(description=DESCRIPTION, epilog=EPILOG,
+  formatter_class=CustomFormatter)
+
+parser.add_argument('input_files', nargs='+')
+parser.add_argument('-v', '--verbose', action='store_true',
+    help='Set logging level to DEBUG')
+
+args = parser.parse_args()
+
+if args.verbose:
+  l.setLevel(logging.DEBUG)
+
+debug('%s begin', SCRIPT_PATH)

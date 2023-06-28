8e6cd5c6db256d8cc0dbcb16a17e052d1a60ca3a
Justin Caringal <jacfriday10th@gmail.com>
2023-06-28 10:33:17 -0500

    Argparse boiler plate

---
+++ implement_model.py
@@ -0,0 +1,50 @@
+# Justin Caringal
+# Tests HDF5 model generated from tensorflow
+
+import os, sys; sys
+import argparse
+import logging
+
+import discord
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import tensorflow as tf
+
+import typing
+from tensorflow import keras as K
+if typing.TYPE_CHECKING:
+    from keras.api._v2 import keras
+    
+    
+
+SCRIPT_PATH = os.path.abspath(__file__)
+FORMAT = '[%(asctime)s] %(levelname)s %(message)s'
+l = logging.getLogger()
+lh = logging.StreamHandler()
+lh.setFormatter(logging.Formatter(FORMAT))
+l.addHandler(lh)
+l.setLevel(logging.INFO)
+debug = l.debug; info = l.info; warning = l.warning; error = l.error
+
+DESCRIPTION = '''
+'''
+
+EPILOG = '''
+'''
+
+class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
+    argparse.RawDescriptionHelpFormatter):
+  pass
+parser = argparse.ArgumentParser(description=DESCRIPTION, epilog=EPILOG,
+  formatter_class=CustomFormatter)
+
+parser.add_argument('input_files', nargs='+')
+parser.add_argument('-v', '--verbose', action='store_true',
+    help='Set logging level to DEBUG')
+
+args = parser.parse_args()
+
+if args.verbose:
+  l.setLevel(logging.DEBUG)
+
+debug('%s begin', SCRIPT_PATH)

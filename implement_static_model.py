f2a6d0613cd77c0aa7555dc42a28f1dd5fbce845
Justin Caringal <jacfriday10th@gmail.com>
2023-07-14 11:44:16 -0500

    Rough Draft of photo-input implementation

--- implement_static_model.py
+++ implement_static_model.py
@@ -7,20 +7,22 @@ import sys
 import numpy as np
 from tensorflow.keras.preprocessing import image
 from keras.models import load_model
-from keras.backend import set_session
 import tensorflow as tf
 
+
+# generate file paths
 model_path = os.path.join('saved_models',
                           'outputs_2023-07-12-123433',
                           'saved_model_2023-07-12-123433.h5')
+img_path = os.path.join('..', 'real_world_data', 'WIN_20230714_10_44_20_Pro.jpg')
 
-test_img = os.path.join('..', 'real_world_data', 'WIN_20230714_10_44_20_Pro.jpg')
-
+# load model from location
 model = load_model(model_path)
 
-print(model.shape)
-sys.exit()
+# load in image, convert to array
+test_image = image.load_img(img_path, target_size = (100, 100))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis=0)
 
-result = model.predict(test_img)
-
-print(result)
+result = model.predict(test_image)
+print(result) 
